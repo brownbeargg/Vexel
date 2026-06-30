8ebb71d5e29b39bebaa344073235fc9fde7efccf
binarybear <mikailoudem@gmail.com>
2026-06-30 19:05:56 +0200

    A QUAD???

--- Vexis/Source/Vexis/EditorLayer.hpp
+++ Vexis/Source/Vexis/EditorLayer.hpp
@@ -12,22 +12,16 @@ namespace Vex
             m_Shader =
                 Shader::Create(RootDirectory::Assets, "Shaders/Basic.vert.spv", "Shaders/Basic.frag.spv");
 
-            // vertices = {
-            //     {{-0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}}, // Bottom left
-            //     {{0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},  // Bottom Right
-            //     {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}}, // Top right
-            //     {{-0.5f, -0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}} // Top left
-            // };
-
             std::vector<VertexData> vertices = {
-                {glm::vec3{-0.5f, 0.5f, 0.0f}, glm::vec3{1.0f, 0.0f, 0.0f}}, // Bottom left
-                {glm::vec3{0.5f, 0.5f, 0.0f}, glm::vec3{0.0f, 1.0f, 0.0f}},  // Bottom right
-                {glm::vec3{0.0f, -0.5f, 0.0f}, glm::vec3{0.0f, 0.0f, 1.0f}}  // Top
+                {{-0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}}, // Bottom left
+                {{0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},  // Bottom Right
+                {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}}, // Top right
+                {{-0.5f, -0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}} // Top left
             };
 
             m_VertexBuffer = VertexBuffer::Create(vertices);
 
-            std::vector<u32> indices = {0, 1, 2};
+            std::vector<u32> indices = {0, 1, 2, 0, 2, 3};
             m_IndexBuffer = IndexBuffer::Create(indices);
         }
 
